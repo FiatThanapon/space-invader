3647de5a1d01898f6cf466efcb253e1c851fbb96
ธนพล อั๋งสกุล <thanaponaungsakul@gmail.com>
2024-02-18 03:36:34 +0700

    def collides function

--- main.py
+++ main.py
@@ -9,6 +9,22 @@ Builder.load_file('design.kv')
 
 Window.size = (500, 700)
 
+def collides(rect1,rect2):
+    r1x = rect1[0][0]
+    r1y = rect1[0][1]
+    r2x = rect2[0][0]
+    r2y = rect2[0][1]
+    r1w = rect1[1][0]
+    r1h = rect1[1][1]
+    r2w = rect2[1][0]
+    r2h = rect2[1][1]
+
+    if ( r1x > r2w and r1x + r1w > r2x and r1y < r2y + r2h and r1y + r1h > r2h):
+        return True
+    else:
+        return False
+
+
 class Alien(Widget):
     pass
 
